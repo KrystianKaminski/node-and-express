75294fe52aea6a18bbd5c80599ea8bfe36a9c388
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-31 12:00:30 +0100

    Improve layout

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("home");
-});
-
-router.get("/contact", (req, res) => {
-  res.render("contact");
+router.get('/', (req, res) => {
+   res.render('home');
 });
 
 module.exports = router;
