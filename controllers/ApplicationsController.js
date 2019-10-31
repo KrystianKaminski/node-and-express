84ca5a73bb3b8b52b0d156c584d74e8eb443b402
KrystianKaminski <krystiankaminski07@gmail.com>
2019-10-31 12:31:36 +0100

    Example of middleware

--- controllers/ApplicationsController.js
+++ controllers/ApplicationsController.js
@@ -1,4 +1,14 @@
 exports.store = (req, res) => {
-  req.flash("form", req.body.name.split(" ")[0] + ", you are a true hero!");
+  req.flash("form", req.body.first_name + ", you are a true hero!");
   res.redirect("/");
+};
+
+exports.normalizeData = (req, res, next) => {
+  const nameArr = req.body.name.split(" ");
+  // Imie Nazwisko =? ['Imie', 'Naziwsko']
+
+  req.body.first_name = nameArr.shift();
+  req.body.last_name = nameArr.join(" ");
+
+  next();
 };
